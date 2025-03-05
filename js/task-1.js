45e39735315e17b3006a2bda65e129bca0c2b896
yusuf sengoz <sengozyusuf91@gmail.com>
2025-03-05 22:26:51 +0300

    Dosyalar eklendi

---
+++ js/task-1.js
@@ -0,0 +1,16 @@
+function isEnoughCapacity(products, containerSize) {
+  let toplam = 0;
+  for (const a in products) {
+    toplam += products[a];
+  }
+
+  return toplam < containerSize ? true : false;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
